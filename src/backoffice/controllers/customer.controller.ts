7f71e4341751a9f9eb79bf092a311b49cfe7c25f
edvilson <edvilson.ads@gmail.com>
2020-09-23 07:40:04 -0300

    Criação de rotas de API

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,25 +1,32 @@
-import{ Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import{ Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Customer } from '../models/customer.model';
+import { Result } from '../models/result.model';
 
-//localhost:3000/customer
+//localhost:3000/customers
 @Controller('v1/customers') //Base da Url
 export class CustomerController {
     @Get()
     get(){
-        return 'Obter cliente';
+        return new Result(null, true, [], null);
+    }
+
+    @Get(':document')
+    getById(@Param('document') document){
+        return new Result(null, true, {}, null);
     }
 
     @Post()
-    post(){
-        return 'Criar cliente';
+    post(@Body() body: Customer){
+        return new Result('Cliente criado com sucesso', true, body, null);
     }
 
-    @Put()
-    put(){
-        return 'Atualizar cliente';
+    @Put(':document')
+    put(@Param('document') document, @Body() body){
+        return new Result('Cliente atualizado com sucesso', true, body, null);
     }
 
-    @Delete()
-    delete(){
-        return 'Deletar cliente';
+    @Delete(':document')
+    delete(@Param('document') document, @Body() body){
+        return new Result('Cliente removido com sucesso', true, body, null);
     }
 }
